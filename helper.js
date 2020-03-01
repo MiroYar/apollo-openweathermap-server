c3c82970a4dd38d84f66c38d3e7a14e355bc0d45
MiroYar <MiroYar@users.noreply.github.com>
2020-03-01 17:48:48 +0700

    Update helper.js
    Added support for cyrillic characters in the request

--- helper.js
+++ helper.js
@@ -3,7 +3,9 @@ const axios = require('axios');
 const API_KEY = 'fc9e50ba954c37424ef33df15255ca1f';
 
 const fetchWeather = async ({ cityName, countryCode }) => {
-    const url = `http://api.openweathermap.org/data/2.5/forecast?q=${cityName},${countryCode}&appid=${API_KEY}`;
+    const url = `http://api.openweathermap.org/data/2.5/forecast?q=${encodeURIComponent(
+        cityName
+    )},${encodeURIComponent(countryCode)}&appid=${API_KEY}`;
     return axios.get(url).then(response => response.data);
 };
 const avg = (obj, total) => obj.reduce((a, b) => a + b, 0) / total;
